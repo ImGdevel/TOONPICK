3b552d5b7cb2a93657afb647987a7e54d55d898c
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2024-08-04 11:24:05 +0900

    스프링 시큐리티 JWT 기본 설정
    스프링 시큐리티 기본 설정

---
+++ backend/src/main/java/toonpick/app/controller/MainController.java
@@ -0,0 +1,16 @@
+package toonpick.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class MainController {
+
+    @GetMapping("/")
+    public String mainPage(){
+        return "main page";
+    }
+
+}
