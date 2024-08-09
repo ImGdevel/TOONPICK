053e1d3b18815b69c8cc73b350947db0dd7b5146
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2024-08-09 12:06:46 +0900

    CORS 설정

---
+++ backend/src/main/java/toonpick/app/config/CorsMvcConfig.java
@@ -0,0 +1,19 @@
+package toonpick.app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .exposedHeaders("Set-Cookie")
+                .allowCredentials(true);
+    }
+}
--- backend/src/main/java/toonpick/app/config/SecurityConfig.java
+++ backend/src/main/java/toonpick/app/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package toonpick.app.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -9,13 +10,18 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.client.web.OAuth2LoginAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import toonpick.app.jwt.JWTFilter;
 import toonpick.app.jwt.JWTUtils;
 import toonpick.app.jwt.LoginFilter;
 import toonpick.app.oauth2.CustomSuccessHandler;
 import toonpick.app.service.OAuth2UserService;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -46,7 +52,25 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf((csrf) -> csrf.disable())
+                .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+
+                            @Override
+                            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                                CorsConfiguration configuration = new CorsConfiguration();
+
+                                configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                                configuration.setAllowedMethods(Collections.singletonList("*"));
+                                configuration.setAllowedHeaders(Collections.singletonList("*"));
+                                configuration.setAllowCredentials(true);
+                                configuration.setMaxAge(3600L);
+
+                                configuration.setExposedHeaders(Collections.singletonList("Set-Cookie"));
+                                configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+                                return configuration;
+                            }
+                        }))
                 .formLogin((formLogin) -> formLogin.disable())
                 .httpBasic((httpBasic) -> httpBasic.disable())
                 .oauth2Login((oauth2) -> oauth2
@@ -59,6 +83,7 @@ public class SecurityConfig {
                         .anyRequest().authenticated())
                 .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtils), UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(new JWTFilter(jwtUtils), UsernamePasswordAuthenticationFilter.class)
+                .addFilterAfter(new JWTFilter(jwtUtils), OAuth2LoginAuthenticationFilter.class)
                 .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
