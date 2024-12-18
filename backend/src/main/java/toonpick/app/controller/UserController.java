1ed51f6cf2b581de954cfc786b1aad6aef234640
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2024-12-18 16:35:58 +0900

    fix(UserController): Authentication에서 유저 정보 추출

--- backend/src/main/java/toonpick/app/controller/UserController.java
+++ backend/src/main/java/toonpick/app/controller/UserController.java
@@ -3,11 +3,12 @@ package toonpick.app.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import toonpick.app.dto.CustomUserDetails;
 import toonpick.app.dto.UserDTO;
-import toonpick.app.security.jwt.JwtTokenProvider;
 import toonpick.app.service.UserService;
 
 @RestController
@@ -15,22 +16,17 @@ import toonpick.app.service.UserService;
 public class UserController {
 
     private final UserService userService;
-    private final JwtTokenProvider jwtTokenProvider;
 
     @Autowired
-    public UserController(UserService userService, JwtTokenProvider jwtTokenProvider) {
+    public UserController(UserService userService) {
         this.userService = userService;
-        this.jwtTokenProvider = jwtTokenProvider;
-    }
-
-    private Long extractUserIdFromToken(HttpServletRequest request) {
-        String token = jwtTokenProvider.resolveToken(request);
-        return jwtTokenProvider.getUserId(token);
     }
 
     @GetMapping("/profile")
-    public ResponseEntity<UserDTO> getUserProfile(HttpServletRequest request) {
-        Long userId = extractUserIdFromToken(request);
+    public ResponseEntity<UserDTO> getUserProfile(HttpServletRequest request, Authentication authentication) {
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+        Long userId = userDetails.getUserId();
+
         UserDTO userDTO = userService.getUserById(userId);
         return ResponseEntity.ok(userDTO);
     }
