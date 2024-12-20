a64a45ce371a21433aaa54e89b5082ce87105138
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2024-12-20 22:56:26 +0900

    refactory(OAuth2UserService): SRP에 따른 기능 분리

--- backend/src/main/java/toonpick/app/service/OAuth2UserService.java
+++ backend/src/main/java/toonpick/app/service/OAuth2UserService.java
@@ -1,5 +1,6 @@
 package toonpick.app.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
@@ -7,68 +8,72 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 import toonpick.app.dto.*;
 import toonpick.app.entity.User;
+import toonpick.app.mapper.UserMapper;
 import toonpick.app.repository.UserRepository;
 
-import java.util.Optional;
+import java.util.Map;
 
 @Service
+@RequiredArgsConstructor
 public class OAuth2UserService extends DefaultOAuth2UserService {
 
     private final UserRepository userRepository;
-
-    public OAuth2UserService(UserRepository userRepository){
-        this.userRepository = userRepository;
-    }
+    private final UserMapper userMapper;
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User oAuth2User = super.loadUser(userRequest);
-        System.out.println(oAuth2User);
-
         String registrationID = userRequest.getClientRegistration().getRegistrationId();
 
-        OAuth2Response oAuth2Response = null;
-        if (registrationID.equals("naver")) {
-            oAuth2Response = new NaverResponse(oAuth2User.getAttributes());
-
-        } else if (registrationID.equals("google")) {
-            oAuth2Response = new GoogleResponse(oAuth2User.getAttributes());
-        } else {
-            return null;
+        OAuth2Response oAuth2Response = createOAuth2Response(registrationID, oAuth2User.getAttributes());
+        if (oAuth2Response == null) {
+            throw new OAuth2AuthenticationException("Unsupported provider: " + registrationID);
         }
 
-        // 리소스 서버에서 발급 받은 정보로 사용자를 특정할 아이디값을 만듬
-        String username = oAuth2Response.getProvider() + " " + oAuth2Response.getProviderId();
+        User user = getOrCreateUser(oAuth2Response);
+        UserDTO userDTO = userMapper.userToUserDto(user);
 
-        Optional<User> existDataOptional = userRepository.findByUsername(username);
+        return new CustomOAuth2User(userDTO);
+    }
 
-        if (existDataOptional.isEmpty()) {
-            User user = User.builder()
-                    .username(username)
-                    .password("")
-                    .email(oAuth2Response.getEmail())
-                    .nickname(oAuth2Response.getName())
-                    .profilePicture("default_profile_img.png")
-                    .isAdultVerified(false)
-                    .role("ROLE_USER")
-                    .build();
-
-            userRepository.save(user);
-
-            UserDTO userDTO = new UserDTO();
-            userDTO.setUsername(username);
-            userDTO.setRole("ROLE_USER");
-
-            return new CustomOAuth2User(userDTO);
-        } else {
-            User existData = existDataOptional.get();
-            userRepository.save(existData);
-
-            UserDTO userDTO = new UserDTO();
-            userDTO.setUsername(existData.getUsername());
-            userDTO.setRole(existData.getRole());
-
-            return new CustomOAuth2User(userDTO);
+    // 서비스 제공자에 따른 OAuth2Response 생성
+    private OAuth2Response createOAuth2Response(String registrationID, Map<String, Object> attributes) {
+        switch (registrationID.toLowerCase()) {
+            case "naver":
+                return new NaverResponse(attributes);
+            case "google":
+                return new GoogleResponse(attributes);
+            default:
+                return null;
         }
     }
+
+    // DB에 User 등록
+    private User getOrCreateUser(OAuth2Response oAuth2Response) {
+        String username = generateUsername(oAuth2Response);
+
+        return userRepository.findByUsername(username).orElseGet(() -> {
+            User newUser = createUser(oAuth2Response, username);
+            return userRepository.save(newUser);
+        });
+    }
+
+    // UserName 생성
+    private String generateUsername(OAuth2Response oAuth2Response) {
+        return oAuth2Response.getProvider() + " " + oAuth2Response.getProviderId();
+    }
+
+    // User 등록
+    private User createUser(OAuth2Response oAuth2Response, String username) {
+        return User.builder()
+                .username(username)
+                .password("")
+                .email(oAuth2Response.getEmail())
+                .nickname(oAuth2Response.getName())
+                .profilePicture("default_profile_img.png")
+                .isAdultVerified(false)
+                .role("ROLE_USER")
+                .build();
+    }
+
 }
