442b719ed8644248518b250c8f66386ad3b531d9
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2025-01-03 21:56:03 +0900

    feat(Icon ) : front

---
+++ frontend/src/components/PublisherIcon.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { ReactComponent as HomeIcon } from './icons/home.svg';
+import { ReactComponent as SearchIcon } from './icons/search.svg';
+import { ReactComponent as UserIcon } from './icons/user.svg';
+
+// 아이콘 맵핑
+const ICON_MAP = {
+  home: HomeIcon,
+  search: SearchIcon,
+  user: UserIcon,
+};
+
+const Icon = ({ name }) => {
+  const SelectedIcon = ICON_MAP[name];
+
+  if (!SelectedIcon) {
+    return <div style={{ width: '25px', height: '25px' }}>Invalid Icon</div>;
+  }
+
+  return <SelectedIcon style={{ width: '25px', height: '25px' }} />;
+};
+
+export default Icon;
