3adb9badaff43aa4343b0baae5429c75faa7c002
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2025-01-06 14:35:52 +0900

    feat(JwtTokenValidator): JWT 유효성 검증에 예외 처리 세분화

--- backend/src/main/java/toonpick/app/auth/jwt/JwtTokenValidator.java
+++ backend/src/main/java/toonpick/app/auth/jwt/JwtTokenValidator.java
@@ -6,6 +6,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+import toonpick.app.auth.exception.ExpiredJwtTokenException;
+import toonpick.app.auth.exception.InvalidJwtTokenException;
+import toonpick.app.auth.exception.MissingJwtTokenException;
 import toonpick.app.auth.user.CustomUserDetails;
 
 @Component
@@ -16,19 +19,19 @@ public class JwtTokenValidator {
 
     // Access Token 추출
     public String extractAccessToken(String authorizationHeader) {
-        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-            return authorizationHeader.substring(7).trim();
+        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+            throw new MissingJwtTokenException("Authorization header is missing or malformed");
         }
-        return null;
+        return authorizationHeader.substring(7).trim();
     }
 
     // Access Token 검증
     public void validateAccessToken(String token) {
         if (jwtTokenProvider.isExpired(token)) {
-            throw new IllegalArgumentException("Access token expired");
+            throw new ExpiredJwtTokenException("Access token expired");
         }
         if (!"access".equals(jwtTokenProvider.getCategory(token))) {
-            throw new IllegalArgumentException("Invalid access token category");
+            throw new InvalidJwtTokenException("Invalid access token category");
         }
     }
 
@@ -48,18 +51,19 @@ public class JwtTokenValidator {
     // Refresh Token 검증
     public void validateRefreshToken(String refreshToken) {
         if (refreshToken == null || refreshToken.isEmpty()) {
-            throw new IllegalArgumentException("Refresh token is missing.");
+            throw new MissingJwtTokenException("Refresh token is missing.");
         }
 
         if (jwtTokenProvider.isExpired(refreshToken)) {
-            throw new IllegalArgumentException("Refresh token expired.");
+            throw new ExpiredJwtTokenException("Refresh token expired.");
         }
 
         if (!"refresh".equals(jwtTokenProvider.getCategory(refreshToken))) {
-            throw new IllegalArgumentException("Invalid refresh token category.");
+            throw new InvalidJwtTokenException("Invalid refresh token.");
         }
     }
 
+    // 유저 정보 추출
     public UserDetails getUserDetails(String token) {
         String username = jwtTokenProvider.getUsername(token);
         String role = jwtTokenProvider.getRole(token);
