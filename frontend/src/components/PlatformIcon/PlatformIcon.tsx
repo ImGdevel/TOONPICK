1036485d7b86107929d00ad5bc89960d500eb231
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2025-01-07 15:57:47 +0900

    feat : add platform icon

--- frontend/src/components/PlatformIcon/PlatformIcon.tsx
+++ frontend/src/components/PlatformIcon/PlatformIcon.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import styles from './PlatformIcon.module.css';
+import { Platform } from '@models/enum';
 
 interface PlatformIconProps {
-  platform: string;
+  platform: Platform;
   size?: 'small' | 'medium' | 'large';
 }
 
 const PlatformIcon: React.FC<PlatformIconProps> = ({ platform, size = 'medium' }) => {
   return (
     <img
-      src={`/images/platforms/${platform.toLowerCase()}.png`}
+      src={`/images/platforms/btn_${platform.toLowerCase()}.svg`}
       alt={`${platform} 플랫폼`}
       className={`${styles.platformIcon} ${styles[size]}`}
     />
