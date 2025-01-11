9d7b9413a80c22c9382b0d67fa924a598b12b56c
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2025-01-11 23:51:12 +0900

    feat : add spinner component

---
+++ frontend/src/components/Spinner/Spinner.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './Spinner.module.css'; // CSS 모듈 경로를 적절히 수정하세요.
+
+const Spinner: React.FC = () => {
+  return <div className={styles.spinner}></div>;
+};
+
+export default Spinner; 
