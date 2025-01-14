a8fd349f4fd72f492dfdf5206fa9efd573aa9ad4
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2025-01-14 23:47:13 +0900

    feat : add achievement item component

---
+++ frontend/src/components/AchievementItem/AchievementItem.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './AchievementItem.module.css';
+
+interface AchievementItemProps {
+  title: string;
+  count: number;
+}
+
+const AchievementItem: React.FC<AchievementItemProps> = ({ title, count }) => {
+  return (
+    <div className={styles.achievementItem}>
+      <div className={styles.details}>
+        <span className={styles.title}>{title}</span>
+        <span className={styles.count}>{count}</span>
+      </div>
+    </div>
+  );
+};
+
+export default AchievementItem; 
