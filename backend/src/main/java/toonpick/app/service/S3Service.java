1bedda662b2303fde6faa065744f88e0dcad4e66
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2025-01-21 19:06:28 +0900

    refactor: 버킷 이름 주입 변경

--- backend/src/main/java/toonpick/app/service/S3Service.java
+++ backend/src/main/java/toonpick/app/service/S3Service.java
@@ -2,6 +2,7 @@ package toonpick.app.service;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -14,13 +15,17 @@ import java.util.UUID;
 
 
 @Service
-@RequiredArgsConstructor
 public class S3Service {
 
     private final AmazonS3 amazonS3;
 
-    @Value("${cloud.aws.s3.bucket}")
     private final String bucketName;
+
+    public S3Service(AmazonS3 amazonS3,
+                     @Value("${cloud.aws.s3.bucket}") String bucketName) {
+        this.amazonS3 = amazonS3;
+        this.bucketName = bucketName;
+    }
 
     /**
      * aws s3에 image upload (범용)
