4aec4118fbe05de197e31cd1427720a10cb25062
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2025-01-22 12:22:45 +0900

    refactor(BookmarkToonController): write swagger docs

--- backend/src/main/java/toonpick/app/controller/BookmarkToonController.java
+++ backend/src/main/java/toonpick/app/controller/BookmarkToonController.java
@@ -1,5 +1,8 @@
 package toonpick.app.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -15,6 +18,7 @@ import toonpick.app.utils.AuthenticationUtil;
 
 import java.util.List;
 
+@Tag(name = "Bookmark Toon", description = "회원 북마크 웹툰 (접근 권한 : Private)")
 @RestController
 @RequestMapping("/api/secure/bookmarks")
 @RequiredArgsConstructor
@@ -23,20 +27,31 @@ public class BookmarkToonController {
     private final BookmarkToonsService bookmarkToonsService;
     private final AuthenticationUtil authenticationUtil;
 
+    @Operation(summary = "북마크 웹툰 추가", description = "회원의 북마크 웹툰 리스트에 해당 웹툰을 추가합니다")
     @PostMapping("/{webtoonId}")
-    public ResponseEntity<Boolean> addBookmark(@RequestParam Long webtoonId, Authentication authentication) {
+    public ResponseEntity<Boolean> addBookmark(
+            @Parameter(description = "리스트에 추가할 웹툰 id")
+            @RequestParam Long webtoonId,
+            Authentication authentication
+    ) {
         String username = authenticationUtil.getUsernameFromAuthentication(authentication);
         boolean result = bookmarkToonsService.addBookmarkToons(username, webtoonId);
         return ResponseEntity.ok(result);
     }
 
+    @Operation(summary = "북마크 웹툰 제거", description = "회원의 북마크 웹툰 리스트에 해당 웹툰을 제거합니다")
     @DeleteMapping("/{webtoonId}")
-    public ResponseEntity<Boolean> removeBookmark(@RequestParam Long webtoonId, Authentication authentication) {
+    public ResponseEntity<Boolean> removeBookmark(
+            @Parameter(description = "리스트에 제거할 웹툰 id")
+            @RequestParam Long webtoonId,
+            Authentication authentication
+    ) {
         String username = authenticationUtil.getUsernameFromAuthentication(authentication);
         boolean result = bookmarkToonsService.removeBookmarkToons(username, webtoonId);
         return ResponseEntity.ok(result);
     }
 
+    @Operation(summary = "북마크 웹툰 리스트 조회", description = "현재 회원의 북마크 웹툰 리스트를 조회합니다")
     @GetMapping
     public ResponseEntity<List<Webtoon>> getBookmarks(Authentication authentication) {
         String username = authenticationUtil.getUsernameFromAuthentication(authentication);
