b844fc56f4dbf4e856528a8fb5312fb18fdfd63c
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2025-01-25 21:21:15 +0900

    feat: 사용자 반응성 수정
    - 소셜 로그인 시 form에서 필요 요건 메시지 가 뜨는 문제점 해결
    - 프로필 위젯이 제대로 닫기지 않는 문제점 해결

--- frontend/src/components/ProfileWidget/ProfileWidget.tsx
+++ frontend/src/components/ProfileWidget/ProfileWidget.tsx
@@ -21,8 +21,13 @@ const ProfileWidget: React.FC<ProfileWidgetProps> = ({
   profileButtonRef,
   profileWidgetRef,
 }) => {
-  const handleButtonClick = (action: () => void) => {
+  const handleButtonClick = (action: () => void, closeWidget: () => void) => (event: React.MouseEvent) => {
+    event.stopPropagation();
     action();
+    closeWidget();
+  };
+
+  const closeProfileWidget = () => {
     setProfileWidgetOpen(false);
   };
 
@@ -31,7 +36,7 @@ const ProfileWidget: React.FC<ProfileWidgetProps> = ({
     const isProfileWidgetClick = profileWidgetRef.current && profileWidgetRef.current.contains(event.target as Node);
 
     if (!isProfileClick && !isProfileWidgetClick) {
-      setProfileWidgetOpen(false);
+      closeProfileWidget();
     }
   }, [profileButtonRef, profileWidgetRef, setProfileWidgetOpen]);
 
@@ -60,25 +65,25 @@ const ProfileWidget: React.FC<ProfileWidgetProps> = ({
         <p>{memberProfile?.nickname || '게스트 사용자'}</p>
       </div>
       <button
-        onClick={() => handleButtonClick(() => onNavigate('/mypage'))}
+        onClick={handleButtonClick(() => onNavigate('/mypage'), closeProfileWidget)}
         className={styles.widgetButton}
       >
         마이페이지
       </button>
       <button
-        onClick={() => handleButtonClick(() => onNavigate('/profile-edit'))}
+        onClick={handleButtonClick(() => onNavigate('/profile-edit'), closeProfileWidget)}
         className={styles.widgetButton}
       >
         프로필 수정
       </button>
       <button
-        onClick={() => handleButtonClick(() => onNavigate('/my-webtoons'))}
+        onClick={handleButtonClick(() => onNavigate('/my-webtoons'), closeProfileWidget)}
         className={styles.widgetButton}
       >
         나의 웹툰 리스트
       </button>
       <button
-        onClick={() => handleButtonClick(onLogout)}
+        onClick={handleButtonClick(onLogout, closeProfileWidget)}
         className={styles.widgetButton}
       >
         로그아웃
