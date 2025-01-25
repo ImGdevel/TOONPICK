9a5a2eb89b79a8df02bff850e34e10846fd903c8
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2025-01-25 12:58:56 +0900

    feat(WebtoonRepository): 업데이트를 요청 쿼리 정의

--- backend/src/main/java/toonpick/app/repository/WebtoonRepository.java
+++ backend/src/main/java/toonpick/app/repository/WebtoonRepository.java
@@ -6,11 +6,25 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 import toonpick.app.domain.webtoon.Webtoon;
+import toonpick.app.dto.webtoon.WebtoonUpdateRequest;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 public interface WebtoonRepository extends JpaRepository<Webtoon, Long>, WebtoonRepositoryCustom {
    Optional<Webtoon> findByExternalId(String externalId);
+
+   // 쿼리 설명 : 연재중인 웹툰중 업데이트 요일이거나 휴재중인 웹툰 중 오늘 업데이트 되지 않은 웹툰 반환
+    @Query("SELECT new com.example.dto.WebtoonUpdateRequest(w.id, w.title, w.link, w.platform, w.serializationStatus) " +
+           "FROM Webtoon w " +
+           "WHERE w.lastUpdatedDate < :today " +
+           "AND (" +
+           "    (w.serializationStatus = 'ONGOING' AND w.dayOfWeek = :tomorrow) " +
+           "    OR w.serializationStatus = 'HIATUS'" +
+           ")")
+    List<WebtoonUpdateRequest> findWebtoonsForUpdate(@Param("today") LocalDate today, @Param("tomorrow") DayOfWeek tomorrow);
 
     @Modifying
     @Transactional
