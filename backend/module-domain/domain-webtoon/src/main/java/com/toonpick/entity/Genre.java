fc1e92816d19907bcfb490f30420f84e2a4c6160
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2025-04-20 13:35:51 +0900

    refactory: Genre 도메인 재정의
    - 도메인 정의서에 따른 Genre 도메인 필드 추가
    (type, display)
    
    - 필드 값 수정 메서드 추가

--- backend/module-domain/domain-webtoon/src/main/java/com/toonpick/entity/Genre.java
+++ backend/module-domain/domain-webtoon/src/main/java/com/toonpick/entity/Genre.java
@@ -1,5 +1,6 @@
 package com.toonpick.entity;
 
+import com.toonpick.enums.GenreType;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
@@ -21,12 +22,33 @@ public class Genre {
     @Column(name = "name", nullable = false, unique = true)
     private String name;
 
+    @NotNull
+    @Column(name = "type", nullable = false)
+    private GenreType type = GenreType.SUB;
+
+    @NotNull
+    @Column(name = "display", nullable = false)
+    private boolean display = false;
+
     @Builder
     public Genre(String name) {
         this.name = name;
     }
 
-    public void update(String name) {
-        this.name = name;
+    public void promoteToMain() {
+        this.type = GenreType.MAIN;
+        this.display = true;
+    }
+
+    public void demoteToSub() {
+        this.type = GenreType.SUB;
+    }
+
+    public void enableDisplay() {
+        this.display = true;
+    }
+
+    public void disableDisplay() {
+        this.display = false;
     }
 }
