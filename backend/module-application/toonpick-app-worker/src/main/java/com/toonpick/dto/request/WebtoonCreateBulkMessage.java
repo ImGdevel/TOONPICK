37a0ea02822606ed51fd342266a655361eab9e1c
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2025-04-25 21:45:29 +0900

    feat : message -> sqs로 전달 가능한 데이터로 수정
    인터페이스 SQSMessage 를 상속 받고 toJson구현

--- backend/module-application/toonpick-app-worker/src/main/java/com/toonpick/dto/request/WebtoonCreateBulkMessage.java
+++ backend/module-application/toonpick-app-worker/src/main/java/com/toonpick/dto/request/WebtoonCreateBulkMessage.java
@@ -1,5 +1,7 @@
 package com.toonpick.dto.request;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.toonpick.message.SQSMessage;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -7,9 +9,20 @@ import java.util.List;
 
 @Getter
 @Builder
-public class WebtoonCreateBulkMessage {
+public class WebtoonCreateBulkMessage implements SQSMessage {
     private int size;
     private List<WebtoonCreatePayload> requests;
     private String requestTime;
+
+    @Override
+    public String toJson() {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            return objectMapper.writeValueAsString(this);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "{}";
+        }
+    }
 
 }
--- backend/module-application/toonpick-app-worker/src/main/java/com/toonpick/dto/request/WebtoonUpdateBulkMessage.java
+++ backend/module-application/toonpick-app-worker/src/main/java/com/toonpick/dto/request/WebtoonUpdateBulkMessage.java
@@ -1,5 +1,7 @@
 package com.toonpick.dto.request;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.toonpick.message.SQSMessage;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -7,8 +9,19 @@ import java.util.List;
 
 @Getter
 @Builder
-public class WebtoonUpdateBulkMessage {
+public class WebtoonUpdateBulkMessage implements SQSMessage {
     private int size;
     private List<WebtoonUpdatePayload> requests;
     private String requestTime;
+
+    @Override
+    public String toJson() {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            return objectMapper.writeValueAsString(this);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "{}";
+        }
+    }
 }
