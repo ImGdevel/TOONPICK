ee9e47fd712ee4f8bc33b252f97542ad2729e38c
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2025-04-26 11:35:40 +0900

    feat: 임시 로그인 로직 (추후 삭제 요망)
    - 임시 로그인 조직 (추후 축사하고 새로 교체할 것)

---
+++ backend/module-application/toonpick-app-admin/src/main/java/com/toonpick/auth/handler/LoginSuccessHandlerImpl.java
@@ -0,0 +1,43 @@
+package com.toonpick.auth.handler;
+
+import com.toonpick.handler.LoginSuccessHandler;
+import com.toonpick.jwt.TokenIssuer;
+import com.toonpick.utils.CookieUtils;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor
+public class LoginSuccessHandlerImpl extends SimpleUrlAuthenticationSuccessHandler implements LoginSuccessHandler {
+
+    private final TokenIssuer tokenIssuer;
+
+    private static final Logger logger = LoggerFactory.getLogger(LoginSuccessHandlerImpl.class);
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+        String username = authentication.getName();
+        String role = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining());
+
+        String accessToken = tokenIssuer.issueAccessToken(username, role);
+        String refreshToken = tokenIssuer.issueRefreshToken(username, role);
+
+        response.setHeader("Authorization", "Bearer " + accessToken);
+        response.addCookie(CookieUtils.createRefreshCookie(refreshToken));
+        response.setStatus(HttpStatus.OK.value());
+        logger.info("USER LOGIN SUCCESS (username-{})", username);
+    }
+}
