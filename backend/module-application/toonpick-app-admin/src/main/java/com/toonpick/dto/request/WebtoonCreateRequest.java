124b8b3984ffd3e5442a203782373721eb21ae5f
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2025-04-26 17:22:11 +0900

    feat: 웹툰 생성 요청 포맷 정의

--- backend/module-application/toonpick-app-admin/src/main/java/com/toonpick/dto/request/WebtoonCreateRequest.java
+++ backend/module-application/toonpick-app-admin/src/main/java/com/toonpick/dto/request/WebtoonCreateRequest.java
@@ -1,10 +1,63 @@
 package com.toonpick.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.toonpick.enums.AgeRating;
+import com.toonpick.enums.AuthorRole;
+import com.toonpick.enums.Platform;
+import com.toonpick.enums.SerializationStatus;
 import lombok.Builder;
 import lombok.Getter;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.Set;
 
 @Getter
 @Builder
 public class WebtoonCreateRequest {
-    Long id;
+
+    @JsonProperty("title")
+    private String title;
+
+    @JsonProperty("external_id")
+    private String externalId;
+
+    @JsonProperty("platform")
+    private Platform platform;
+
+    @JsonProperty("day_of_week")
+    private DayOfWeek dayOfWeek;
+
+    @JsonProperty("thumbnail_url")
+    private String thumbnailUrl;
+
+    @JsonProperty("link")
+    private String link;
+
+    @JsonProperty("age_rating")
+    private AgeRating ageRating;
+
+    @JsonProperty("description")
+    private String summary;
+
+    @JsonProperty("serialization_status")
+    private SerializationStatus serializationStatus;
+
+    @JsonProperty("episode_count")
+    private int episodeCount;
+
+    @JsonProperty("platform_rating")
+    private float platformRating;
+
+    @JsonProperty("publish_start_date")
+    private LocalDate publishStartDate;
+
+    @JsonProperty("last_updated_date")
+    private LocalDate lastUpdatedDate;
+
+    @JsonProperty("authors")
+    private Set<AuthorRequest> authors;
+
+    @JsonProperty("genres")
+    private Set<String> genres;
 }
