13e7a34845fb6693851f92490724378ee2bd74ea
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2025-04-27 21:20:53 +0900

    feat: auth server logic
    - auth 관련 로직 이동

---
+++ backend/module-application/toonpick-app-auth/src/main/java/com/toonpick/auth/service/UsernameLoginService.java
@@ -0,0 +1,30 @@
+package com.toonpick.auth.service;
+
+import com.toonpick.entity.Member;
+import com.toonpick.repository.MemberRepository;
+import com.toonpick.type.ErrorCode;
+import com.toonpick.user.CustomUserDetails;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UsernameLoginService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Member member = memberRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(ErrorCode.MEMBER_NOT_FOUND.getMessage() + username));
+        return CustomUserDetails.builder()
+                .username(member.getUsername())
+                .password(member.getPassword())
+                .role(String.valueOf(member.getRole()))
+                .build();
+    }
+}
