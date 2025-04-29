174175db1ebf1df1779a3a75beea95757cf91210
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2025-04-29 15:15:45 +0900

    feat: web-client 방식으로 변경
    - 기존 restTemplate방식은 동기 방식
    - 비동기 방식인 webclient 방식으로 변경

--- backend/module-internal/event-webhook/src/main/java/com/toonpick/internal/webhook/slack/SlackNotifier.java
+++ backend/module-internal/event-webhook/src/main/java/com/toonpick/internal/webhook/slack/SlackNotifier.java
@@ -1,12 +1,12 @@
 package com.toonpick.internal.webhook.slack;
 
+import com.toonpick.type.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
-import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
@@ -14,16 +14,19 @@ public class SlackNotifier {
 
     final private Logger logger = LoggerFactory.getLogger(SlackNotifier.class);
 
-    private final SlackProperties slackProperties;
-
-    private final RestTemplate restTemplate;
+    private final WebClient webClient;
 
     public void send(String message) {
+        SlackMessage payload = new SlackMessage(message);
         try {
-            Map<String, String> payload = Map.of("text", message);
-            restTemplate.postForEntity(slackProperties.getUrl(), payload, String.class);
+            webClient.post()
+                .bodyValue(payload)
+                .retrieve()
+                .bodyToMono(String.class)
+                .doOnError(e -> logger.error(ErrorCode.SLACK_SEND_FAIL.getMessage()))
+                .subscribe();
         } catch (Exception e) {
-            logger.error("Slack 메시지 전송 실패", e);
+            throw new RuntimeException(ErrorCode.SLACK_SEND_FAIL.getMessage());
         }
     }
 }
--- backend/module-internal/event-webhook/src/main/java/com/toonpick/internal/webhook/slack/WebhookConfig.java
+++ backend/module-internal/event-webhook/src/main/java/com/toonpick/internal/webhook/slack/WebhookConfig.java
@@ -2,12 +2,24 @@ package com.toonpick.internal.webhook.slack;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebhookConfig {
+
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public WebClient webClient(WebClient.Builder builder, SlackProperties slackProperties) {
+        return builder
+                .baseUrl(slackProperties.getUrl())
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
     }
 }
