0e91bbe43ed6bf03400616e5fe358af7449322f3
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2025-05-06 18:45:08 +0900

    refactory: 이제 jwt에러는 handler가 처리

--- backend/module-application/toonpick-app-api/src/main/java/com/toonpick/auth/controller/TokenReissueController.java
+++ backend/module-application/toonpick-app-api/src/main/java/com/toonpick/auth/controller/TokenReissueController.java
@@ -32,33 +32,23 @@ public class TokenReissueController {
 
     private static final Logger logger = LoggerFactory.getLogger(TokenReissueController.class);
 
-    // todo : Refresh 토큰 발급 로직 Service로 분리 + AOP 적용할 것 : Controller는 오직 전달 역활만 간단하게!
-
     @Operation(summary = "access 토큰 재발급", description = "refresh 토큰을 통해 access 토큰 재발급")
     @PostMapping("/reissue")
     public ResponseEntity<String> reissue(HttpServletRequest request, HttpServletResponse response) {
-        try {
-            // 쿠키에서 refresh token 추출 및 검증
-            String refreshToken = jwtTokenValidator.extractRefreshTokenFromCookies(request);
-            jwtTokenValidator.validateRefreshToken(refreshToken);
+        // 쿠키에서 refresh token 추출 및 검증
+        String refreshToken = jwtTokenValidator.extractRefreshTokenFromCookies(request);
+        jwtTokenValidator.validateRefreshToken(refreshToken);
 
-            // 새로운 Access 토큰 발급
-            String newAccessToken = tokenIssuer.reissueAccessToken(refreshToken);
-            response.setHeader("Authorization", "Bearer " + newAccessToken);
+        // 새로운 Access 토큰 발급
+        String newAccessToken = tokenIssuer.reissueAccessToken(refreshToken);
+        response.setHeader("Authorization", "Bearer " + newAccessToken);
 
-            // Refresh 토큰 갱신 필요 여부 확인 및 갱신
-            if (jwtTokenProvider.isRefreshTokenAboutToExpire(refreshToken)) {
-                String newRefreshToken = tokenIssuer.reissueRefreshToken(refreshToken);
-                response.addCookie(CookieUtils.createRefreshCookie(newRefreshToken));
-            }
-
-            return new ResponseEntity<>(HttpStatus.OK);
-        } catch (ExpiredJwtTokenException | InvalidJwtTokenException | MissingJwtTokenException e) {
-            logger.error("Token validation error: {}", e.getMessage());
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
-        } catch (RuntimeException e) {
-            logger.error("Unexpected error: {}", e.getMessage());
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        // Refresh 토큰 갱신 필요 여부 확인 및 갱신
+        if (jwtTokenProvider.isRefreshTokenAboutToExpire(refreshToken)) {
+            String newRefreshToken = tokenIssuer.reissueRefreshToken(refreshToken);
+            response.addCookie(CookieUtils.createRefreshCookie(newRefreshToken));
         }
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
