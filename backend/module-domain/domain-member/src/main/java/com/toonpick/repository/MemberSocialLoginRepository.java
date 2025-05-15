8668378468f3bc1eccbac4223e359693dab30fa0
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2025-05-15 22:46:40 +0900

    refactory: 소셜 로그인 로직 수정

--- backend/module-domain/domain-member/src/main/java/com/toonpick/repository/MemberSocialLoginRepository.java
+++ backend/module-domain/domain-member/src/main/java/com/toonpick/repository/MemberSocialLoginRepository.java
@@ -1,7 +1,12 @@
 package com.toonpick.repository;
 
 import com.toonpick.entity.MemberSocialLogin;
+import com.toonpick.enums.Provider;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberSocialLoginRepository extends JpaRepository<MemberSocialLogin, Long> {
+
+    Optional<MemberSocialLogin> findByProviderAndProviderId(Provider provider, String providerId);
 }
