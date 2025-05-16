eca7fc8a220faf64c274efa9c23888eab26d051b
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2025-05-16 19:48:43 +0900

    feat: webtoon platform repository 등록

---
+++ backend/module-domain/domain-webtoon/src/main/java/com/toonpick/repository/WebtoonPlatformRepository.java
@@ -0,0 +1,8 @@
+package com.toonpick.repository;
+
+import com.toonpick.entity.WebtoonPlatform;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WebtoonPlatformRepository extends JpaRepository<WebtoonPlatform, Long> {
+
+}
