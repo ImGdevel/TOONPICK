10ef34958c6798d2f9d403418c43c45f34a9936d
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2025-05-17 22:38:33 +0900

    fix: 누락된 id 생성 어노테이션 추가
    Platform에 누락된 @GeneratedValue(strategy = GenerationType.IDENTITY) 어노테이션 추가

--- backend/module-domain/domain-webtoon/src/main/java/com/toonpick/entity/Platform.java
+++ backend/module-domain/domain-webtoon/src/main/java/com/toonpick/entity/Platform.java
@@ -4,6 +4,7 @@ package com.toonpick.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
@@ -21,7 +22,7 @@ import lombok.NoArgsConstructor;
 public class Platform {
 
     @Id
-    @GeneratedValue()
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "name", nullable = false, unique = true, length = 30)
