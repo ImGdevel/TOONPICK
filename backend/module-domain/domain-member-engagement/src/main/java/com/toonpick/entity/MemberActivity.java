075dbe36e353c5897c5db2d643438ec3291375d6
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2025-05-19 16:56:52 +0900

    feat: 맴버 활동 로그 엔티티 추가

---
+++ backend/module-domain/domain-member-engagement/src/main/java/com/toonpick/entity/MemberActivity.java
@@ -0,0 +1,38 @@
+package com.toonpick.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "member_activity")
+public class MemberActivity extends BaseTimeEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "reward_id", nullable = false)
+    private ActivityReward reward;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "activity_type", nullable = false)
+    private String activityType;
+}
