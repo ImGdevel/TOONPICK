382ed2a924637511ce2d3bcb6e438968ba86f630
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2025-05-20 15:04:38 +0900

    feat: 웹툰 추천 도메인 추가 및 분리

---
+++ backend/module-domain/domain-webtoon-recommend/src/main/java/com/toonpick/repository/SimilarWebtoonRepository.java
@@ -0,0 +1,7 @@
+package com.toonpick.repository;
+
+import com.toonpick.entity.SimilarWebtoon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SimilarWebtoonRepository extends JpaRepository<SimilarWebtoon, Long> {
+}
