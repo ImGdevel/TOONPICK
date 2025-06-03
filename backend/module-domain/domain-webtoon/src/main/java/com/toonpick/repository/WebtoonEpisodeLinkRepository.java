1cd68770413c900bda1a49b1d7d63ea87379de0b
WooSH <imdlsrks.mc@gmail.com>
2025-06-03 20:35:42 +0900

    feat: webtoon repository 추가
    AuthorSNSRepository
    WebtoonEpisodeLinkRepository
    WebtoonEpisodeRepository

---
+++ backend/module-domain/domain-webtoon/src/main/java/com/toonpick/repository/WebtoonEpisodeLinkRepository.java
@@ -0,0 +1,7 @@
+package com.toonpick.repository;
+
+import com.toonpick.entity.WebtoonEpisodeLink;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WebtoonEpisodeLinkRepository extends JpaRepository<WebtoonEpisodeLink, Long> {
+}
---
+++ backend/module-domain/domain-webtoon/src/main/java/com/toonpick/repository/WebtoonEpisodeRepository.java
@@ -0,0 +1,8 @@
+package com.toonpick.repository;
+
+import com.toonpick.entity.WebtoonEpisode;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WebtoonEpisodeRepository extends JpaRepository<WebtoonEpisode, Long> {
+
+}
