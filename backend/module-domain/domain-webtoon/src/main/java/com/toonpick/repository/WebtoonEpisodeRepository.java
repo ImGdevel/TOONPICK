8bc0599be5e4fcce50a5c907afbaf83199e5afae
WooSH <imdlsrks.mc@gmail.com>
2025-06-03 21:09:59 +0900

    feat: webtoon episode repository에 엔티티 조회 추가
    웹툰 별 에피소드 조회 쿼리
    시즌 별 에피소드 조회 쿼리

--- backend/module-domain/domain-webtoon/src/main/java/com/toonpick/repository/WebtoonEpisodeRepository.java
+++ backend/module-domain/domain-webtoon/src/main/java/com/toonpick/repository/WebtoonEpisodeRepository.java
@@ -3,6 +3,17 @@ package com.toonpick.repository;
 import com.toonpick.entity.WebtoonEpisode;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface WebtoonEpisodeRepository extends JpaRepository<WebtoonEpisode, Long> {
 
+    /**
+     * 웹툰 별 에피소드 조회
+     */
+    List<WebtoonEpisode> findByWebtoonId(Long webtoonId);
+
+    /**
+     * 시즌 별 에피소드 조회
+     */
+    List<WebtoonEpisode> findByWebtoonSeasonId(Long seasonId);
 }
