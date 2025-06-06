161d6603d277353fbd8c8149cedad7590e432307
WooSH <imdlsrks.mc@gmail.com>
2025-06-06 14:12:42 +0900

    feat: sqs 이벤트 타입 정의

---
+++ backend/module-application/toonpick-app-worker/src/main/java/com/toonpick/common/type/SQSEventType.java
@@ -0,0 +1,32 @@
+package com.toonpick.common.type;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+public enum SQSEventType {
+
+    CRAWL_WEBTOON_ALL("crawl_webtoon_all"),
+    CRAWL_WEBTOON_EPISODE("crawl_webtoon_episode"),
+    CRAWL_WEBTOON_NEW("crawl_webtoon_new");
+
+    private final String value;
+
+    SQSEventType(String value) {
+        this.value = value;
+    }
+
+    @JsonValue
+    public String getValue() {
+        return value;
+    }
+
+    @JsonCreator
+    public static SQSEventType fromValue(String value) {
+        for (SQSEventType type : SQSEventType.values()) {
+            if (type.value.equalsIgnoreCase(value)) {
+                return type;
+            }
+        }
+        throw new IllegalArgumentException("Unknown SQSEventType: " + value);
+    }
+}
