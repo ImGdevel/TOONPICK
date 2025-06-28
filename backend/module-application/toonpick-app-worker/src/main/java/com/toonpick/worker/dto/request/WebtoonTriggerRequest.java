7da449f7b5b496a57636c0b0a1efc2aa227914d1
WooSH <imdlsrks.mc@gmail.com>
2025-06-28 21:35:27 +0900

    refactor: 웹툰 트리거 요청 및 응답 DTO 구조 변경
    WebtoonTriggerResponse 클래스를 삭제하고, 새로운 WebtoonTriggerResponse 클래스를 response 패키지로 이동하여 성공 여부와 taskId를 포함하도록 수정하였음. 또한, WebtoonTriggerRequest 클래스를 새로 추가하여 웹툰 트리거 요청에 필요한 필드를 정의하였음.

--- backend/module-application/toonpick-app-worker/src/main/java/com/toonpick/worker/dto/request/WebtoonTriggerRequest.java
+++ backend/module-application/toonpick-app-worker/src/main/java/com/toonpick/worker/dto/request/WebtoonTriggerRequest.java
@@ -1,18 +1,19 @@
-package com.toonpick.worker.dto;
+package com.toonpick.worker.dto.request;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+/**
+ * 웹툰 트리거 요청 DTO
+ */
 @Getter
-@Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class WebtoonTriggerRequest {
     private String id;
-    private String webtoon_url;
     private String platform;
+    private String webtoon_url;
 } 
