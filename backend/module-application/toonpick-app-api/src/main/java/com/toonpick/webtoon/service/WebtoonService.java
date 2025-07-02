915f6a7b1f998328af1863325619c710db8055d5
WooSH <imdlsrks.mc@gmail.com>
2025-07-02 16:41:52 +0900

    refactor: WebtoonService 코드 정리 및 로깅 개선
    WebtoonService에서 불필요한 Logger 인스턴스를 제거하고, Lombok의 @Slf4j 어노테이션을 사용하여 로깅을 개선하였음. 또한, 웹툰 상세 데이터 조회 메서드에 @Transactional(readOnly = true) 어노테이션을 추가하여 성능을 최적화하였음.

--- backend/module-application/toonpick-app-api/src/main/java/com/toonpick/webtoon/service/WebtoonService.java
+++ backend/module-application/toonpick-app-api/src/main/java/com/toonpick/webtoon/service/WebtoonService.java
@@ -1,18 +1,17 @@
 package com.toonpick.webtoon.service;
 
 
+import com.toonpick.common.exception.EntityNotFoundException;
+import com.toonpick.common.type.ErrorCode;
 import com.toonpick.domain.dto.PagedResponseDTO;
 import com.toonpick.domain.webtoon.dto.WebtoonFilterDTO;
 import com.toonpick.domain.webtoon.entity.Webtoon;
-import com.toonpick.common.exception.EntityNotFoundException;
 import com.toonpick.domain.webtoon.repository.WebtoonRepository;
-import com.toonpick.common.type.ErrorCode;
 import com.toonpick.webtoon.mapper.WebtoonMapper;
 import com.toonpick.webtoon.response.WebtoonDetailsResponse;
 import com.toonpick.webtoon.response.WebtoonResponse;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -23,14 +22,16 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * Webtoon 관련 비즈니스 로직 서비스
+ */
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class WebtoonService {
 
     private final WebtoonRepository webtoonRepository;
     private final WebtoonMapper webtoonMapper;
-
-    private static final Logger logger = LoggerFactory.getLogger(WebtoonService.class);
 
     /**
      * id 기반으로 웹툰 조회
@@ -45,6 +46,7 @@ public class WebtoonService {
     /**
      * id 기반으로 웹툰 상세 데이터 조회
      */
+    @Transactional(readOnly = true)
     public WebtoonDetailsResponse getWebtoonDetails(Long id) {
         Webtoon webtoon = webtoonRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException(ErrorCode.WEBTOON_NOT_FOUND, String.valueOf(id)));
